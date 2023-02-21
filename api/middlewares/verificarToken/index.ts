d6999374c9a7ffb9e46971d34838c37b5c8903b4
Denilson Rocha <denilsonrocha_@outlook.com>
2023-02-21 00:58:38 -0300

    middleware para verificação do token

---
+++ api/middlewares/verificarToken/index.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express"
+import { verify } from "jsonwebtoken"
+
+function verificarToken(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Response | void {
+  const authHeader = req.headers["authorization"]
+  const token = authHeader && authHeader.split(" ")[1]
+
+  if (!token) {
+    return res.status(401).json({
+      msg: "Acesso negado!",
+    })
+  }
+
+  try {
+    const secret = process.env.SECRET
+
+    verify(token, secret)
+
+    next()
+  } catch (err) {
+    return res.status(400).json({ msg: "Token inválido!" })
+  }
+}
+
+export default verificarToken
