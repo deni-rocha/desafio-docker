6b59c07107be5f3710c50efefc348761b37c6604
Denilson Rocha <denilsonrocha_@outlook.com>
2023-02-21 00:59:10 -0300

    modelagem do usuário

---
+++ api/models/Usuario.ts
@@ -0,0 +1,24 @@
+import { model, Schema } from "mongoose"
+
+export interface IUsuario {
+  nome: string
+  email: string
+  senha: string
+  permissao: string
+}
+
+const usuarioSchema = new Schema<IUsuario>({
+  nome: { type: String, required: true, minlength: 4 },
+  senha: { type: String, required: true },
+  email: { type: String, required: true },
+  permissao: {
+    type: String,
+    enum: ["ADMIN", "USUARIO"],
+    default: "USUARIO",
+    required: true,
+  },
+})
+
+const User = model<IUsuario>("Usuario", usuarioSchema)
+
+export default User
