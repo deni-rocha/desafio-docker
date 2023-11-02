77c0ef6538f0f3adfd2bb1a51196b2e26688b006
Denilson Rocha <denilsondevlinux@gmail.com>
2023-11-01 22:29:02 -0300

    css para funcinamento do swagger na Vercel

--- src/server.ts
+++ src/server.ts
@@ -14,11 +14,19 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+// para o funcionamento do swagger na Vercel
+const CSS_URL =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
+
 // conectar banco de dados
 loaders.connect()
 
 // rotas
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, { customCssUrl: CSS_URL }),
+)
 
 app.use("/aluno", aluno)
 
